6af2a0482da425d013dd44a00bbdb8635541b402
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2025-03-15 18:16:45 +0800

    更新

---
+++ files/js/schedule-data.js
@@ -0,0 +1,59 @@
+// 时间表数据 - 每日更新时间表
+const scheduleData = {
+    monday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/monday/test"
+        },
+    ],
+    tuesday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/tuesday/test"
+        },
+    ],
+    wednesday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/wednesday/test"
+        },
+    ],
+    thursday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/thursday/test"
+        },
+    ],
+    friday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/friday/test"
+        },
+    ],
+    saturday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/saturday/test"
+        },
+    ],
+    sunday: [
+        {
+            image: " ",
+            title: "测试中",
+            time: "更新时间: 22:00",
+            url: "/animation/sunday/test"
+        },
+    ]
+}; 
